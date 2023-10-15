f00521b70f8a27e8a0109cb63534387eddb8249e
Adrijan Mitrov <unstoppablebyte@gmail.com>
2023-10-15 19:05:45 +0200

    add vue configuration

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    plugins: [vue()],
+    build: {
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, 'src/index.js'),
+            name: 'VueModal',
+            // the proper extensions will be added
+            fileName: 'vue-modal',
+            formats: ['es', 'cjs', 'umd'], // Include 'umd' format
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['vue'],
+            output: {
+                format: ['cjs', 'es', 'umd'], // Include 'umd' format
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+        }
+    }
+})
